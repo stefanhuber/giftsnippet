34993b65bc4afcfe6239195420c04d95ce7a116e
Stefan Huber <main@stefanhuber.at>
2021-10-07 16:08:00 +0200

    remove build

--- build/lib/giftsnippet/__init__.py
+++
@@ -1,3 +0,0 @@
-from .gift import find_snippet_lines, replace_lines, get_base_dir, process_gift_file
-from .highlight import get_data_uri_snippet, get_lexer_from_filetype
-from .image import get_data_uri_image, transform_image_to_data_uri
